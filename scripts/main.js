af5aa4fd8ce94fd88836057bcaf50211aa90af65
Goody <mattgoodman3099@gmail.com>
2020-08-20 10:37:17 -0500

    added seed modules and functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,28 @@
 console.log("Welcome to the main module")
 
-import {createPlan} from './plan.js'
-
+import { createPlan } from './plan.js'
+import { createAsparagus } from './seeds/asparagus.js'
+import { createPotato } from './seeds/potato.js'
+import { createSoybean } from './seeds/soybean.js'
+import { createSunflower } from './seeds/sunflower.js'
+import { createWheat } from './seeds/wheat.js'
+import { createCorn } from './seeds/corn.js'
 
 
 const yearlyPlan = createPlan()
 
 console.log(yearlyPlan)
+
+const asparagusSeed = createAsparagus()
+const potatoSeed = createPotato()
+const soybeanSeed = createSoybean()
+const sunflowerSeed = createSunflower()
+const wheatSeed = createWheat()
+const cornSeed = createCorn
+
+console.log(asparagusSeed)
+console.log(potatoSeed)
+console.log(soybeanSeed)
+console.log(sunflowerSeed)
+console.log(wheatSeed)
+console.log(cornSeed)
